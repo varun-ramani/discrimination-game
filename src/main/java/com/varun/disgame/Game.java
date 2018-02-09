0714506719889d86a22085e374e80458e3f0e466
Varun Ramani <varun.ramani@gmail.com>
2018-02-09 12:10:36 -0500

    Started working on the Game class

---
+++ src/main/java/com/varun/disgame/Game.java
@@ -0,0 +1,10 @@
+package com.varun.disgame;
+
+public class Game {
+    static long score = 0;
+    static boolean isRacist;
+
+    public void start() {
+        
+    }
+}
--- src/main/java/com/varun/disgame/Main.java
+++ src/main/java/com/varun/disgame/Main.java
@@ -1,5 +1,9 @@
 package com.varun.disgame;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Un Cuento de Jose");
+        Game game = new Game();
+        game.start();      
+    }
 }
