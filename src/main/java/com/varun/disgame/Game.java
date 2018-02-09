fefd46bd829ef376afdd193022eea0ac3c90c7b4
Varun Ramani <varun.ramani@gmail.com>
2018-02-09 12:25:51 -0500

    Added Player.java mechanism

--- src/main/java/com/varun/disgame/Game.java
+++ src/main/java/com/varun/disgame/Game.java
@@ -1,12 +1,11 @@
 package com.varun.disgame;
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class Game {
-    public static long score = 0;
-    public static boolean isRacist;
-    public ArrayList<Integer> choices = new ArrayList<Integer>();
+   
+    // public ArrayList<Integer> choices = new ArrayList<Integer>();
 
     public void start() {
-        
+               
     }
 }
---
+++ src/main/java/com/varun/disgame/Player.java
@@ -0,0 +1,9 @@
+package com.varun.disgame;
+import java.util.*;
+
+public class Player {
+    public static long score;
+    public static ArrayList<Integer> choices = new ArrayList<Integer>();
+
+    
+}
